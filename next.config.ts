38183d9557ce0e7acef6f85e1f665e11cd50a844
Sami Pippuri <pippuri@gmail.com>
2025-07-21 22:21:35 +0300

    still debugging

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/appspecific/com.tesla.3p.public-key.pem',
+        destination: '/wellknown/appspecific/com.tesla.3p.public-key.txt',
+      },
+      {
+        source: '/.well-known/:path*',
+        destination: '/wellknown/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
